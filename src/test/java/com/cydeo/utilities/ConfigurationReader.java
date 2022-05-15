7af2bbfe5310b6a23d8a4d701e923d74b6b8722b
FeiZhou <sophia08241@gmail.com>
2022-05-15 01:19:01 -0400

    API

--- src/test/java/com/cydeo/utilities/ConfigurationReader.java
+++ src/test/java/com/cydeo/utilities/ConfigurationReader.java
@@ -1,32 +1,27 @@
 package com.cydeo.utilities;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
-/**
- * reads the properties file configuration.properties
- */
 public class ConfigurationReader {
 
-    private static Properties properties;
+    private static Properties configFile;
 
     static {
-
         try {
-            String path = "configuration.properties";
-            FileInputStream input = new FileInputStream(path);
-            properties = new Properties();
-            properties.load(input);
-
-            input.close();
-        } catch (Exception e) {
+            FileInputStream fileInputStream = new FileInputStream("configuration.properties");
+            configFile = new Properties();
+            configFile.load(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            System.out.println("Failed to load properties file!");
             e.printStackTrace();
-
         }
     }
 
-    public static String get(String keyName) {
-        return properties.getProperty(keyName);
+    public static String getProperty(String key) {
+        return configFile.getProperty(key);
     }
 
 }
